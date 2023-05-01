9f909bbb1f035cc380e6772d06d7c086526cb9ab
Tobi <tobias_zoppi@hotmail.com>
2023-05-01 12:47:01 -0300

    Primer Commit

---
+++ src/main/java/com/tobiaszoppi/springfundamentals/SpringFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.tobiaszoppi.springfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalsApplication.class, args);
+	}
+
+}
